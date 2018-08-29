e27cd69ee508c7404d2761f4aab6df9a47b626c9
jeffreypang911 <jeffreypang911@gmail.com>
2018-08-29 20:43:33 +0000

    Adding Count Letters Assignment

--- countLetters.js
+++ countLetters.js
@@ -1,3 +1,24 @@
-function(countLetters) {
+var args = process.argv.slice(2);
+var word = args.join("");
 
+
+function countLetters(word) {
+var arr = {};
+for (var i = 0; i < word.length; i++) {
+    var key = word[i]
+    if (key in arr) {
+        arr[key] += 1;
+      }
+    else {
+      arr[word[i]] = 1;
+
+      }
+  }
+  console.log(arr);
 }
+  countLetters(word);
+
+
+
+
+
